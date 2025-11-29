e02718d72d7405570b76ee9ce4d59a5907d110e9
artur.chernov <volenros@mail.ru>
2025-11-29 18:37:08 +0500

    active test

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,8 +1,10 @@
-package java;
+package test.java;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println(123 + 456);
+        System.out.println("Test Pull request 2");
     }
 }
