7ac6ba0211077d366e637ce4a50c613e761f55ec
artur.chernov <volenros@mail.ru>
2025-11-29 18:13:10 +0500

    Test pull request

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,7 +3,7 @@ package java;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello, World");
+        System.out.println("Test Pull request");
     }
 }
