0e1904900d423d2ef1befbd855c8073ced6165dc
artur.chernov <volenros@mail.ru>
2025-11-30 17:21:54 +0500

    create branch for Pull request

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        System.out.println("Проверяем Pull снова");
-        System.out.println("Test Pull request 3");
+        System.out.println("Начинает злить");
+
+        System.out.println("Измения для pull requests");
     }
 }
